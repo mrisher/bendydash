4cefecfc2343577c6dc71911182e0b6b8fae44dd
Mark Risher <218038+mrisher@users.noreply.github.com>
2025-12-24 16:31:30 +0100

    baseline fork

--- tools/buildTTF.sh
+++
@@ -1,7 +0,0 @@
-FONT='../fonts/Poppins-ExtraBold.ttf'
-FONT2='../fonts/Poppins-Regular.ttf'
-python ttfToEPD.py ../fonts/04B.TTF ../bendydash/font8.cpp 8
-python ttfToEPD.py $FONT ../bendydash/font16.cpp 16
-python ttfToEPD.py $FONT ../bendydash/font36.cpp 36
-python ttfToEPD.py $FONT2 ../bendydash/font38.cpp 38
-python ttfToEPD.py $FONT ../bendydash/font92.cpp 92
